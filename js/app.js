b2d4090d9487278d6c5831730e2bbb9cbd4ce461
Vladislav <testerreality@gmail.com>
2018-05-06 19:45:48 +0300

    Добавлена функция обновления игры

--- js/app.js
+++ js/app.js
@@ -254,3 +254,11 @@ function failSuccess(quiz, userAnswer) {
         });
     }
 }
+
+// Начать заного
+function resetQuiz(quiz) {
+    quiz.randomQuestions = shuffleArr([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    quiz.questionNumber = 0;
+    quiz.correct = 0;
+    quiz.incorrect = 0;
+}
